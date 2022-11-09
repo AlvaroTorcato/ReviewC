a98242a49020d631364583132d868b907d9ec1e6
Álvaro Torcato <alvaromanuelreis@gmail.com>
2022-11-09 09:16:40 +0000

    Initial commit

---
+++ src/main/java/com/example/reviewc/ReviewCApplication.java
@@ -0,0 +1,13 @@
+package com.example.reviewc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewCApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReviewCApplication.class, args);
+    }
+
+}
