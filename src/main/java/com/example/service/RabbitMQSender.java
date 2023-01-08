cd4a6178e1ace75d3334e0007207d90f31b7d85a
TiagoNora <tiagonora2002@gmail.com>
2023-01-08 02:35:12 +0000

    Update

--- src/main/java/com/example/service/RabbitMQSender.java
+++ src/main/java/com/example/service/RabbitMQSender.java
@@ -9,6 +9,7 @@ import com.fasterxml.jackson.databind.ObjectWriter;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.core.FanoutExchange;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,34 +17,43 @@ import org.springframework.stereotype.Service;
 public class RabbitMQSender {
     @Autowired
     private RabbitTemplate rabbitTemplate;
-    public String exchange = "Rev";
-    public String exchangeDelete = "RevDelete";
-    public String exchangeUpdate = "RevUpdate";
-    public String exchangeVoteUpdate = "ReveVoteUpdate";
+
+    @Autowired
+    private FanoutExchange fanout;
+
+    @Autowired
+    private FanoutExchange fanoutDelete;
+
+    @Autowired
+    private FanoutExchange fanoutUpdate;
+
+    @Autowired
+    private FanoutExchange fanoutVoteUpdate;
+
 
     private static Logger logger = LogManager.getLogger(RabbitMQSender.class.toString());
     public void sendJsonMessage(Review review) throws JsonProcessingException {
         ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
         String Json=ow.writeValueAsString(review);
-        rabbitTemplate.convertAndSend(exchange,"",Json);
+        rabbitTemplate.convertAndSend(fanout.getName(),"",Json);
         logger.info("Sending Message to the Queue : " + Json);
     }
     public void sendDelete(int id){
-        rabbitTemplate.convertAndSend(exchangeDelete,"", id);
+        rabbitTemplate.convertAndSend(fanoutDelete.getName(),"", id);
         logger.info("Sending Message to the Queue : " + id);
     }
 
     public void sendUpdate(Change change) throws JsonProcessingException {
         ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
         String Json=ow.writeValueAsString(change);
-        rabbitTemplate.convertAndSend(exchangeUpdate,"", Json);
+        rabbitTemplate.convertAndSend(fanoutUpdate.getName(),"", Json);
         logger.info("Sending Message to the Queue : " + Json);
     }
 
     public void sendVoteUpdate(Votes vote) throws JsonProcessingException {
         ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
         String Json=ow.writeValueAsString(vote);
-        rabbitTemplate.convertAndSend(exchangeVoteUpdate,"", Json);
+        rabbitTemplate.convertAndSend(fanoutVoteUpdate.getName(),"", Json);
         logger.info("Sending Message to the Queue : " + Json);
     }
     /*@Autowired
